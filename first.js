006835e02cc0e78689d73e232e0186cb8af9fcc0
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-07-15 01:09:17 +0600

    print

--- first.js
+++ first.js
@@ -4,3 +4,13 @@ console.log(age)
 name="'tony stark'";
 console.log(name);
 
+// variable
+
+let name2="fathma";
+console.log(name2);
+
+var name3="mm";
+console.log(name3);
+
+const name4="MIm";
+console.log(name4);  
